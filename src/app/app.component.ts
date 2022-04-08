b9f455bff953f912145269a0e10ba5402bd20c6d
LePacko <49279268+LePacko@users.noreply.github.com>
2022-04-08 18:42:36 +0200

    try form submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import {RadioButtonModule} from 'primeng/radiobutton';
 import {CheckboxModule} from 'primeng/checkbox';
 import {InputTextareaModule} from 'primeng/inputtextarea';
 import {InputMaskModule} from 'primeng/inputmask';
+import { NetlifyFormsService } from './netlify-forms/netlify-forms.service';
+import { FormBuilder, Validators } from '@angular/forms';
+
+
 
 
 
@@ -14,6 +18,15 @@ import {InputMaskModule} from 'primeng/inputmask';
 })
 
 export class AppComponent {
+  errorMsg = '';
+  feedbackForm = this.fb.group({
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private netlifyForms: NetlifyFormsService,
+  ) { }
+
   public autoResize: boolean = true;
 
   selectedSize: string='M';
@@ -24,4 +37,26 @@ export class AppComponent {
   selectedPotType:string='';
   textAvis:string='';
   mail:string='';
+
+  onSubmit() {
+    this.feedbackForm = this.fb.group({
+      selectedSize: ['', Validators.required],
+      selectedPlantNumber: ['', Validators.required],
+      boolFigurine: ['',  Validators.required],
+      selectedPlantType: ['', Validators.required],
+      selectedEnvironnement: ['', Validators.required],
+      selectedPotType: ['', Validators.required],
+      textAvis: ['', Validators.required],
+      mail:  ['', [Validators.email, Validators.required]]
+    });
+    this.netlifyForms.submitFeedback(this.feedbackForm.value).subscribe(
+      () => {
+        this.feedbackForm.reset();
+        //this.router.navigateByUrl('/success');
+      },
+      err => {
+        this.errorMsg = err;
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -25,8 +25,12 @@ import {InputMaskModule} from 'primeng/inputmask';
     RadioButtonModule,
     InputTextareaModule,
     InputMaskModule,
-    FormsModule
+    FormsModule, 
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
+  providers: [],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
